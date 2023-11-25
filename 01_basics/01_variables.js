7cbde0578a993c369e59e4485853d5cda0c8d051
SanchitGawande <97868098+SanchitGawande@users.noreply.github.com>
2023-11-25 05:25:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=12345
+let accountEmail = "hitesh#google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId = 2
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
